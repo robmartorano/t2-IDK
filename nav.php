d4250abbaac3428735813bbf862eccb9991d55bb
Andy Chen <ac447@cs.duke.edu>
2015-10-11 03:58:33 -0400

    testing branch

---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav>
+<div class="navigation">
+		
+		
+		<ul class="tabs">
+		<li id="Login" onclick="location.href='aaa.html'">Login</li>
+		<li id="Registration" onclick="location.href='home.html'">Register</li>
+		
+		</ul>
+	</div>
+	<nav>
