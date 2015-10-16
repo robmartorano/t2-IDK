b13061760864de1edea57b2083b9ffd99be45186
Jerry Pan <qp7@cs.duke.edu>
2015-10-16 12:33:52 -0400

    finished registration

--- register.php
+++ register.php
@@ -1,22 +1,27 @@
 <html>
 	<head>
-<link rel="stylesheet" href="stylehomepage.css">
+<link rel="stylesheet" href="styleregister.css">
 	</head>
 
 <body>
 	<header>
 		<?php require_once 'nav.php';?>
-	</header>	
-<div class="forms">
+	</header>
+<div class="text">
+Register to buy textbooks from your classmates at an extremely low price. 
+<br>
+<img class="loginpic" src="dukelogo.jpg" alt="Login" ></img>
+</div>	
+<div class="register">
 <form action="abc.php" method="post">
 
+<br><br>
+First Name:<br> <input class="fname" type="text" name="firstname"><br>
+Last Name:<br> <input class='lname'type="text" name="lastname"><br>
+Password:<br> <input class='pw' type="text" name="password"><br>
+Re-type Password:<br> <input class='rpw' type="text" name="confirmpassword"><br>
 
-First Name: <input type="text" name="firstname"><br>
-Last Name: <input type="text" name="lastname"><br>
-Password: <input type="text" name="password"><br>
-Re-type Password: <input type="text" name="confirmpassword"><br>
-
-E-mail: <input type="text" name="NetID">@duke.edu <br>
+Duke E-mail:<br> <input class='email' type="text" name="NetID"><br><br>
 <input type="submit">
 </div>
 </form>
