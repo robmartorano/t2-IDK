e4c23aba5307e75fe21ffd35ca9ad19426ae6be3
Wilson Zhang <bz43@cs.duke.edu>
2015-10-19 18:41:19 -0400

    Here comes

--- register.php
+++ register.php
@@ -14,12 +14,11 @@ Register to buy textbooks from your classmates at an extremely low price.
 </div>	
 <div class="register">
 <form action="abc.php" method="post">
-
 <br><br>
 First Name:<br> <input class="fname" type="text" name="firstname"><br>
 Last Name:<br> <input class='lname'type="text" name="lastname"><br>
-Password:<br> <input class='pw' type="text" name="password"><br>
-Re-type Password:<br> <input class='rpw' type="text" name="confirmpassword"><br>
+Password:<br> <input class='pw' type="password"  name="password"><br>
+Re-type Password:<br> <input class='rpw' type="password"  name="confirmpassword"><br>
 
 Duke E-mail:<br> <input class='email' type="text" name="NetID"><br><br>
 <input type="submit">
