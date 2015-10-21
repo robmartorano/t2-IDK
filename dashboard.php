0fb677d363fb8695b9ffa7881cc143b252ae09d1
Wilson Zhang <bz43@cs.duke.edu>
2015-10-21 00:37:18 -0400

    dashboard finished not that goodlooking but should be providing the functionality we need

---
+++ dashboard.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="styledashboard.css">
+	</head>
+
+<body>
+	
+	<header>
+		<?php require_once 'nav.php';?>
+	</header>
+		<div class = "left">
+    		<div class = "buy">
+        		<h1 class = "titledash">Books that you want to buy</h1>
+        			<ul>
+        			<li>this should be echo() from the database</li>
+        			<li>a</li>
+        			<li>a</li>
+        			<li>a</li>
+        			</ul>
+    		</div>
+    
+    		<div class = "sell">
+    			<h1 class = "titledash">Books that you want to sell</h1>
+    				<ul>
+        			<li>this should be echo() from the database</li>
+        			<li>a</li>
+        			<li>a</li>
+        			<li>a</li>
+        			</ul>
+    		</div>
+    	</div>
+    	<div class = "right">
+    	
+    		<div class = "displaybook">
+    			<h1 class = "titledash">What is thisssss</h1> 
+    		</div>  
+    	</div>
+      
+
+
+
+</body>
+</html>
