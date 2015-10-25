2569f618c4c2e366b2f3e39b8ecf5ddb9d6f94bd
Jerry Pan <qp7@cs.duke.edu>
2015-10-25 17:47:17 -0400

    idk

--- dashboard.php
+++ dashboard.php
@@ -1,31 +1,64 @@
+<?php session_start() ?>
+
 <html>
 	<head>
 		<link rel="stylesheet" href="styledashboard.css">
+		
 	</head>
 
 <body>
 	
 	<header>
-		<?php require_once 'nav.php';?>
+		<?php require_once 'navcontrol.php';?>
+		
 	</header>
 		<div class = "left">
     		<div class = "buy">
         		<h1 class = "titledash">Books that you want to buy</h1>
-        			<ul>
-        			<li>this should be echo() from the database</li>
-        			<li>a</li>
-        			<li>a</li>
-        			<li>a</li>
-        			</ul>
+				<ul id="what">
+				</ul>
+        			<script type= "text/javascript">
+		 
+		 for (i = 0; i < 5; i++) { 
+			var tr='';
+    // create a new textInputBox  
+           var textInputBox = 'hi';  
+        // create a new Label Text
+            
+            tr += '<li>' + textInputBox + '</li>';  
+			document.write(tr);
+}
+        
+			
+		
+				</script>
+				<li>
+				
+				</li>
+				
     		</div>
     
     		<div class = "sell">
     			<h1 class = "titledash">Books that you want to sell</h1>
     				<ul>
-        			<li>this should be echo() from the database</li>
-        			<li>a</li>
-        			<li>a</li>
-        			<li>a</li>
+        			<script type= "text/javascript">
+		 
+		 for (i = 0; i < 5; i++) { 
+			var tr='';
+    // create a new textInputBox  
+           var textInputBox = 'hi';  
+        // create a new Label Text
+            
+            tr += '<li>' + textInputBox + '</li>';  
+			document.write(tr);
+}
+        
+			
+		
+				</script>
+				<li>
+				add another listing
+				</li>
         			</ul>
     		</div>
     	</div>
