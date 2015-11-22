a6d6435a34709947f5109caa41b1703490a7b405
Wilson Zhang <bz43@cs.duke.edu>
2015-11-22 16:37:00 -0500

    asdfs

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,4 @@
-<?php session_start() 
+<?php session_start()
 $dsn = 'mysql:host=cgi.cs.duke.edu;port=3306;dbname=qp7;';
 $username = 'qp7';
 $password = 'qnDM4.fo6sX_';
@@ -14,7 +14,7 @@ $sqlQ->execute();
 
 
 function getresult($BookName) {
-	return 
+	return
 	"<div class='one_result'>
 		<div class='text_box'>
 			<ul class='text'>
@@ -22,9 +22,9 @@ function getresult($BookName) {
 			</ul>
 		</div>
 	</div>";
-	// still need valid image, edition, seller (with a link to contact),  
+	// still need valid image, edition, seller (with a link to contact),
 	// type, notes, and purchase hyperlink
-	
+
 	// <div>
 	//	<p>$BookName</p>
 	//	<p>$Price</p>
@@ -37,7 +37,7 @@ function getresult($BookName) {
 	  {
 	  $bookid = $row['book_id'] ;
 	  }
-	  
+
 $sqlQ1 = $db->prepare('SELECT * FROM Books WHERE id = :id ');
 	  $sqlQ1->bindValue(':id', $bookid, PDO::PARAM_INT);
 	  $sqlQ1->execute();
@@ -46,7 +46,7 @@ $result = "";
 
  while($row1 = $sqlQ1 -> fetch(PDO::FETCH_ASSOC))
 	  {
-	  
+
 	  $result = $result  + getresult($row1['book_id']) ;
 	  }
 
@@ -71,22 +71,25 @@ $result = "";
 	</head>
 
 <body>
-	
+
 	<header>
 		<?php require_once 'navcontrol.php';?>
-		
+
 	</header>
 		<div id ="popup">
 		<form action="putbook.php"  method="post">
-			<input type="text" name="name" placeholder="Textbook Name"><br>
-			<input type="text" name="price" placeholder="Price"><br>
-			<input type="text" name="ISBN" placeholder="ISBN"><br>
-			<input type="text" name="author" placeholder="author"><br>
-			<input type="text" name="additional" placeholder="a few sentences to describe the book"><br
-			<input type="submit">
-			</form>
+
+<input type="text" name="name" placeholder="Textbook Name"><br>
+<input type="text" name="price" placeholder="Price"><br>
+<input type="text" name="ISBN" placeholder="ISBN"><br>
+<input type="text" name="author" placeholder="author"><br>
+<input type="text" name="additional" placeholder="a few sentences to describe the book"><br>
+
+
+
+
+<input type="submit">
 		</div>
-		
 		<div class = "left">
     		<div class = "buy">
         		<h1 class = "titledash">Books that you want to buy</h1>
@@ -96,44 +99,44 @@ $result = "";
         			<script type= "text/javascript">
 		 var count = "<?php echo $length ?>";
 		 <?php $counter = 0;?>
-		 for (i = 0; i < count; i++) { 
-		 
+		 for (i = 0; i < count; i++) {
+
 			var tr= '';
-    // create a new textInputBox  
-    
-           var textInputBox = "<?php echo $_SESSION['bookname'][$counter] ; $counter++ ?>";  
+    // create a new textInputBox
+
+           var textInputBox = "<?php echo $_SESSION['bookname'][$counter] ; $counter++ ?>";
         // create a new Label Text
-            tr += '<li>' + textInputBox + '</li>';  
+            tr += '<li>' + textInputBox + '</li>';
 			document.write(tr);
 }
-        
-			
-		
+
+
+
 				</script>
 				<li>
-				
+
 				</li>
 				</ul>
-				
+
     		</div>
-    
+
     		<div class = "sell">
     			<h1 class = "titledash">Books that you want to sell</h1>
     				<ul>
         			<script type= "text/javascript">
 		 var num = "<?php Print($Se); ?>";
-		 for (i = 0; i < 5; i++) { 
+		 for (i = 0; i < 5; i++) {
 			var tr='';
-    // create a new textInputBox  
-           var textInputBox = 'hi';  
+    // create a new textInputBox
+           var textInputBox = 'hi';
         // create a new Label Text
-            
-            tr += '<li>' + textInputBox + '</li>';  
+
+            tr += '<li>' + textInputBox + '</li>';
 			document.write(tr);
 }
-        
-			
-		
+
+
+
 				</script>
 				<li id="button">
 				add another listing
@@ -142,12 +145,12 @@ $result = "";
     		</div>
     	</div>
     	<div class = "right">
-    	
+
     		<div class = "displaybook">
-    			<h1 class = "titledash">What is thisssss</h1> 
-    		</div>  
+    			<h1 class = "titledash">What is thisssss</h1>
+    		</div>
     	</div>
-      
+
 
 
 
