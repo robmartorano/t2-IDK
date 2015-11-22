b84a10990617238a0d96278dbc8c34a57bf9df82
Andy Chen <ac447@cs.duke.edu>
2015-11-22 17:57:38 -0500

    nav

--- navcontrol.php
+++ navcontrol.php
@@ -3,18 +3,19 @@
 <nav>
 	<div class="navigation">
 		<ul class="tabs">
+			<li class="filler"></li>
+			<li class="filler"></li>
 			<li id="home" onclick="location.href='index.php'">Duke Textbook Exchange</li>
-			<li></li>
 			<?php
 			if (isset($_SESSION['name'])) {
-				echo "<li id=\"name\" onclick=\"location.href='dashboard.php'\">";
+				echo "<li class=\"filler\" id=\"name\" onclick=\"location.href='dashboard.php'\">";
 					echo $_SESSION['name'];
 					echo "</li>";
-				echo "<li id=\"logout\" onclick=\"location.href='logout.php'\">Logout</li>";
+				echo "<li class=\"filler\" id=\"logout\" onclick=\"location.href='logout.php'\">Logout</li>";
 			} 
 			else {
-				echo "<li id=\"Login\">Login</li>";
-				echo "<li id=\"Registration\" onclick=\"location.href='register.php'\">Sign Up</li>";
+				echo "<li class=\"filler\" id=\"Login\">Login</li>";
+				echo "<li class=\"filler\" id=\"Registration\" onclick=\"location.href='register.php'\">Sign Up</li>";
 			}
 			?>
 		</ul>
--- searchbar.php
+++ searchbar.php
@@ -1,4 +1,7 @@
 <form class="searchbar" action = "Searchresult.php" method="post">
-		<input id="typehere" type="text" placeholder="Enter book's name, author, ISBN, or the class name" required="" name = "wtf">
-		<input id="button" type="submit" value="Search" onclick="location.href='Searchresult.php'">
-		</form>
+	<input id="typehere" type="text" 
+		placeholder="Enter book's name, author, ISBN, or the class name"
+		required="" name = "wtf">
+	<input id="button" type="submit" value="Search" 
+		onclick="location.href='Searchresult.php'">
+</form>
