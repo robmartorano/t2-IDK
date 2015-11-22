c932cfbd6e046c7f0eeb35100923f0408c3a13b3
Andy Chen <ac447@cs.duke.edu>
2015-11-22 17:14:04 -0500

    nav

--- navcontrol.php
+++ navcontrol.php
@@ -1,4 +1,23 @@
-
-<?php if (isset($_SESSION['name'])) {
-   require_once 'nav1.php';} else {
-    require_once 'nav.php';}?>
+<!doctype html>
+<html>
+<nav>
+	<div class="navigation">
+		<ul class="tabs">
+			<li id="home" onclick="location.href='index.php'">Duke Textbook Exchange</li>
+			<li></li>
+			<?php
+			if (isset($_SESSION['name'])) {
+				echo "<li id=\"name\" onclick=\"location.href='dashboard.php'\">";
+					echo $_SESSION['name'];
+					echo "</li>";
+				echo "<li id=\"logout\" onclick=\"location.href='logout.php'\">Logout</li>";
+			} 
+			else {
+				echo "<li id=\"Login\">Login</li>";
+				echo "<li id=\"Registration\" onclick=\"location.href='register.php'\">Sign Up</li>";
+			}
+			?>
+		</ul>
+	</div>
+</nav>
+</html>
