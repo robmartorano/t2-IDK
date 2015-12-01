54e1ffbcc45308614ce9f40cde5b51ee6b4b6762
Jerry Pan <qp7@cs.duke.edu>
2015-11-30 23:14:50 -0500

    hi

--- searchbar.php
+++ searchbar.php
@@ -1,9 +1,7 @@
-<div id="searchwrapper">
 <form class="searchbar" action = "Searchresult.php" method="post">
 	<input id="typehere" type="text" 
 		placeholder="Enter book's name, author or ISBN"
 		 required="" name = "wtf">
 	<input id="button" type="submit" value="Search" 
-		 onclick="checkTextField('typehere');" onclick="location.href='Searchresult.php'">
+		 onclick="checkTextField('typehere');" >
 </form>
-</div>
