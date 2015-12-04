1958793cb4dfd8543483cb9d4d568758a03ccfc4
Jerry Pan <qp7@cs.duke.edu>
2015-12-03 22:01:13 -0500

    sad

--- navcontrol.php
+++ navcontrol.php
@@ -19,8 +19,12 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="login.php">Log In</a></li>
-        <li><a href="register.php">Sign Up</a></li>
+	  <?php if(isset($_SESSION['logged_in'])){
+			echo' <li><a href="dasboard.php">'.$_SESSION["name"].'</a></li>
+        <li><a href="logout.php">Log Out</a></li>';
+		}else{echo'<li><a href="register.php">Sign Up</a></li>
+        <li><a href="login.php">Log In</a></li>';}?>
+        
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
